e2db23b27951c3fc6ceccc1676a145077508db5c
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-10-23 05:44:54 +0200

    create migrations for tables

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 const postRoute = require("./routes/posts");
 
+app.use(bodyParser.json());
 app.use("/posts", postRoute);
 // app.get("/", (req, res) => {
 //   res.send("hello world");
